1bcf96b38663682e5a1261fe7040f151183fd0a3
Maxigameshd <Maxigameshd@online.de>
2024-12-07 21:54:48 +0100

    setup

--- src/main/java/net/maxigames/armor_extras/ArmorExtras.java
+++ src/main/java/net/maxigames/armor_extras/ArmorExtras.java
@@ -2,6 +2,7 @@ package net.maxigames.armor_extras;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.maxigames.armor_extras.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,8 +10,12 @@ public class ArmorExtras implements ModInitializer {
 	public static final String MOD_ID = "armor-extras";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+
 	@Override
 	public void onInitialize() {
-
+		ModItems.initialize();
 	}
+
+
+
 }
